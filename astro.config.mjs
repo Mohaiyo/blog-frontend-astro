cbd0e399e3ee8820dc034d36b27826f8b3c24b53
Wayne Liang <wayne.liang@originlinks.com>
2023-10-17 21:45:23 +0800

    feat: add tailwind.config.cjs and vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mohaiyo.netlify.app"
+  site: 'https://mohaiyo.netlify.app',
+  integrations: [tailwind(), vue()]
 })
